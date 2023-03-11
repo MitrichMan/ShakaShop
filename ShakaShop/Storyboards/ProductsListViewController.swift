85ce8b0ca0d84a56c42e4f140a11ec88904f74b1
AssanCred <temix@live.ru>
2023-03-11 15:43:29 +0700

    firstRelease

---
+++ ShakaShop/Storyboards/ProductsListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProductsListViewController.swift
+//  ShakaShop
+//
+//  Created by Артём Латушкин on 11.03.2023.
+//
+
+import UIKit
+
+class ProductsListViewController: UITableViewController {
+    
+    var products: [Product] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        products.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellProduct", for: indexPath)
+        var
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
---
+++ ShakaShop/ViewControllers/DetailsAboutProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsAboutProductViewController.swift
+//  ShakaShop
+//
+//  Created by Артём Латушкин on 11.03.2023.
+//
+
+import UIKit
+
+class DetailsAboutProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
