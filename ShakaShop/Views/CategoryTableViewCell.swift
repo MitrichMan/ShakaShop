5c7654164b7df8489f65cc74e88b94c42a8b3cb8
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-03-14 13:34:21 +0800

    Categories design

--- ShakaShop/Views/CategoryTableViewCell.swift
+++ ShakaShop/Views/CategoryTableViewCell.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class CategoryTableViewCell: UITableViewCell {
 
+    @IBOutlet var categoryImageView: UIImageView!
+    @IBOutlet var categoryLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
